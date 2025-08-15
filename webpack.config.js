aca5d855ce5cc388d2af2d7befec6df3f95d67f7
mowslasher83 <radeswaled@hotmail.com>
2025-08-15 07:02:32 -0500

    Add comments for better clarity in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
 const path = require('path');
 
 module.exports = {
+  // Entry point of the application
   entry: './src/index.js',
+  // Output configuration
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Directory for output files
+    filename: 'bundle.js', // Name of the output file
   },
+  // Module configuration
   module: {
     rules: [{
       test: /.js$/, // Chỉ áp dụng cho file .js
-      exclude: /node_modules/,
+      exclude: /node_modules/, // Loại bỏ thư mục node_modules
       use: {
-        loader: 'babel-loader',
+        loader: 'babel-loader', // Sử dụng babel-loader để biên dịch
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'] // Tùy chọn cho babel
         }
       }
     }]
   },
+  // Development server configuration
   devServer: {
-    static: './dist',
+    static: './dist', // Thư mục phục vụ tĩnh
   },
 };
